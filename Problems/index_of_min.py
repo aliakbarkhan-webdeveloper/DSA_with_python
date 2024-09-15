34237ed9b57acc8a95b7f5ea3c2bcdd70c1ae5c3
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-09-16 01:22:04 +0500

    commit

---
+++ Problems/index_of_min.py
@@ -0,0 +1,19 @@
+li=[222,1288,139]
+def mini(l):
+    if l.__len__()==0:
+        return None
+    min=0
+    max=1
+    res=0
+    while max<l.__len__():
+        if l[min] < l[max]:
+            res=min
+
+        else:
+            res=max
+            min=max
+        max+=1
+    print("Position:",res+1)
+    return (res+1,li[res])
+
+print(mini(li))
