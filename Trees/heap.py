0a1c3d8b8668715d98514648069e1ab891189bf4
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-09-20 23:02:38 +0500

    commit

--- Trees/heap.py
+++ Trees/heap.py
@@ -23,4 +23,28 @@ buildHeap(heap)
 print(heap)
 
 
+# code for min heap
 
+def min_heapify(arr,n,root ):
+
+    smallest=root
+    left=2*root+1
+    right=2*root+2
+    if left<n and arr[left]<arr[smallest]:
+        smallest=left
+    if right<n and arr[right]<arr[smallest]:
+        smallest=right
+    if smallest!=root:
+        arr[root],arr[smallest]=arr[smallest],arr[root]
+
+        min_heapify(arr,n,smallest)
+
+def build_min_heap(arr):
+    n=len(arr)
+    for i in reversed(range(n//2)):
+        min_heapify(arr,n,i)
+
+build_min_heap(heap)
+print(heap)
+
+#Heap sort
