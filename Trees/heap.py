8a59130c2c43500ebb12ac469c8221be6e35ade7
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-09-20 19:27:39 +0500

    max heap

--- Trees/heap.py
+++ Trees/heap.py
@@ -1,14 +1,26 @@
-def heapify(arr,n,root ):
+def max_heapify(arr,n,root ):
 
     largest=root
     left=2*root+1
     right=2*root+2
-    if arr<n and arr[left]>arr[largest]:
-        largest=arr
+    if left<n and arr[left]>arr[largest]:
+        largest=left
     if right<n and arr[right]>arr[largest]:
         largest=right
     if largest!=root:
         arr[root],arr[largest]=arr[largest],arr[root]
 
-        heapify(arr,n,largest)
+        max_heapify(arr,n,largest)
+
+def buildHeap(arr):
+    n=len(arr)
+    for i in reversed(range(n//2)):
+        max_heapify(arr,n,i)
+
+
+heap=[100,5,3,2,8,15,6,102]
+buildHeap(heap)
+print(heap)
+
+
 
